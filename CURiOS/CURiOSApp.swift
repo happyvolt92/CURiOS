334e33cf6e38d93a11bb723936386ab9aa0a6499
Happyvolt92 <elodiegage@gmail.com>
2024-02-08 20:43:02 +0100

    Initial Commit

---
+++ CURiOS/CURiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CURiOSApp.swift
+//  CURiOS
+//
+//  Created by HappyVolt on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CURiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
